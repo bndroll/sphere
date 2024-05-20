e2b8cc8a3bf68aa23852b0270ff9dfcd8de6f949
Nikita Smirnov <smirnov@aeroidea.ru>
2024-05-21 03:55:29 +0400

    fix; hardcode recsys

--- recsys/main.py
+++ recsys/main.py
@@ -4,7 +4,7 @@ import tensorflow_text
 import uuid
 import numpy as np
 
-embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3")
+#embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3")
 
 app = Flask(__name__)
 
@@ -12,10 +12,10 @@ app = Flask(__name__)
 def get_embeddings():
     data = request.json
 
-    data["sentence"]
-    embedding = embed([sentence])[0].numpy().tolist()
-    vector = np.linalg.norm(np.array(embedding))
-    result = {"id": data["id"], "embedding": vector}
+#    data["sentence"]
+#    embedding = embed([sentence])[0].numpy().tolist()
+#    vector = np.linalg.norm(np.array(embedding))
+    result = {"id": data["id"], "embedding": 0}
 
     return jsonify(result)
 
